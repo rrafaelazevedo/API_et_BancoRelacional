629b8206fd41f52c2ce2684327f49fe0321f7d15
RAFAEL AZEVEDO DE SOUZA <47624259826@info.local>
2024-10-16 22:11:43 -0300

    consultar_cadastrar_atualizar_deletar

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ app = Flask('carros')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # configurações do database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:senai%40134@127.0.0.1/aula_25_09_24_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:senai%40134@127.0.0.1/bd_carro'
 
 # declaração/configuracao da 'variável' que recebera o database
 mybd = SQLAlchemy(app)
 
 # definição da estrutura da tabela de carros
 class Carros(mybd.Model):
+    __tablename__ = 'tb_carro'
     id =     mybd.Column(mybd.Integer, primary_key = True)
     marca =  mybd.Column(mybd.String(100))
     modelo = mybd.Column(mybd.String(100))
@@ -29,15 +30,15 @@ class Carros(mybd.Model):
     numero_vendas =  mybd.Column(mybd.Float)
     ano =    mybd.Column(mybd.String())
 
-# convertendo a tabela em JSON
-def to_json(self):
-    return{'id': self.id, 
-           'marca': self.marca,
-           'modelo': self.modelo,
-           'valor': self.valor,
-           'cor': self.cor,
-           'numero_vendas': self.numero_vendas,
-           'ano': self.ano}
+    # convertendo a tabela em JSON
+    def to_json(self):
+        return{'id': self.id, 
+            'marca': self.marca,
+            'modelo': self.modelo,
+            'valor': self.valor,
+            'cor': self.cor,
+            'numero_vendas': self.numero_vendas,
+            'ano': self.ano}
 
 # ****** API ******
 # selecionar tudo (GET)
@@ -85,14 +86,47 @@ def atualizar_carro(id):
 
     # consulta por id 
     carro_objetos = Carros.query.filter_by(id=id).first()
+
+    # corpo da requisição
     body = request.get_json()
 
     try:
         if 'marca' in body:
             carro_objetos.marca = body['marca']
+        if 'modelo' in body:
+            carro_objetos.modelo = body['modelo']
+        if 'valor' in body:
+            carro_objetos.valor = body['valor']
+        if 'cor' in body:
+            carro_objetos.cor = body['cor']
+        if 'numero_vendas' in body:
+            carro_objetos.numero_vendas = body['numero_vendas']
+        if 'ano' in body:
+            carro_objetos.ano = body['ano']
 
+        mybd.session.add(carro_objetos)
+        mybd.session.commit()
 
+        return gera_response(200, 'carros', carro_objetos.to_json(), 'Atualização realizada com sucesso!')
+    
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, 'carros', {}, 'ERRO ao atualizar database!')
 
+# deletar carro do database
+@app.route('/carros/<id>', methods=['DELETE'])
+def deletar_carro(id):
+    carro_objetos = Carros.query.filter_by(id=id).first()
+
+    try:
+        mybd.session.delete(carro_objetos)
+        mybd.session.commit()
+
+        return gera_response(200, 'carros', carro_objetos.to_json(), 'Deletado com sucesso!')
+    
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, 'carros', {}, 'ERRO ao deletar!')
 
 
 
@@ -103,3 +137,6 @@ def gera_response(status, nome_database, conteudo_database, mensagem = False):
     if mensagem:
         body['mensagem'] = mensagem
     return Response(json.dumps(body), status=status, mimetype='application/json')   
+
+app.run(port=5000, host='localhost', debug=True)
+
